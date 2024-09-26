2324833f0945c43109780c62084903b1ecf739dd
Kalana Mihiranga <kalanamihiranga97@gmail.com>
2024-09-27 01:04:15 +0530

    feat:Create Data Transfer Objects and Conversions

--- src/main/java/com/example/ScheduleMate/controller/EventController.java
+++ src/main/java/com/example/ScheduleMate/controller/EventController.java
@@ -1,7 +1,12 @@
 package com.example.ScheduleMate.controller;
 
+import com.example.ScheduleMate.dto.EventDto;
+import com.example.ScheduleMate.dto.clientDto;
+import com.example.ScheduleMate.endpoints.APIResponse;
 import com.example.ScheduleMate.entity.Event;
 import com.example.ScheduleMate.service.EventService;
+import com.example.ScheduleMate.utils.ResponseCode;
+import jakarta.validation.constraints.Null;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,13 +20,15 @@ public class EventController {
     private EventService eventService;
 
     @PostMapping("/{providerId}/availability")
-    public ResponseEntity<Event> postAvailability(@PathVariable Long providerId, @RequestBody Event event) {
+    public ResponseEntity<APIResponse<Null>> postAvailability(@PathVariable Long providerId, @RequestBody EventDto event) {
 //        event.setProviderId(providerId);
-        return ResponseEntity.ok(eventService.postAvailabilityy(event));
+        eventService.postAvailabilityy(event);
+
+        return ResponseEntity.ok(new APIResponse<>(ResponseCode.SUCCESS));
     }
 
     @GetMapping("/availability")
-    public ResponseEntity<List<Event>> getAvailability() {
-        return ResponseEntity.ok(eventService.getAvailability());
+    public ResponseEntity<APIResponse<List<EventDto>>> getAvailability() {
+        return ResponseEntity.ok(new APIResponse<>(ResponseCode.SUCCESS,eventService.getAvailability()));
     }
 }
--- src/main/java/com/example/ScheduleMate/service/impl/EventServiceImpl.java
+++ src/main/java/com/example/ScheduleMate/service/impl/EventServiceImpl.java
@@ -1,26 +1,32 @@
 package com.example.ScheduleMate.service.impl;
 
+import com.example.ScheduleMate.dto.EventDto;
 import com.example.ScheduleMate.entity.Event;
 import com.example.ScheduleMate.repository.EventRepository;
 import com.example.ScheduleMate.service.EventService;
+import com.example.ScheduleMate.utils.converters.EventDtoUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class EventServiceImpl implements EventService {
     private final EventRepository eventRepository;
 
     @Override
-    public Event postAvailabilityy(Event event) {
+    public void postAvailabilityy(EventDto event) {
+        Event events = EventDtoUtils.EVENT_DTO_TO_EVENT_FUNCTION.apply(event);
 
-        return eventRepository.save(event);
+         eventRepository.save(events);
     }
 
     @Override
-    public List<Event> getAvailability() {
+    public List<EventDto> getAvailability() {
 
-        return eventRepository.findAll();
+        return eventRepository.findAll().stream().
+                map(e->EventDtoUtils.EVENT_TO_EVENT_DTO_FUNCTION.apply(e)).collect(Collectors.toList());
     }
 }
