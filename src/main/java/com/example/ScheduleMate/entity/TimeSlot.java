9a9d219efaba447ba9652cc888c060f304bdd666
Kalana Mihiranga <kalanamihiranga97@gmail.com>
2024-10-15 01:26:04 +0530

    feat:update the entities with relationship

--- src/main/java/com/example/ScheduleMate/entity/TimeSlot.java
+++ src/main/java/com/example/ScheduleMate/entity/TimeSlot.java
@@ -18,10 +18,13 @@ public class TimeSlot extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(name="start_time")
     private LocalDateTime startTime;
+
     @Column(name="end_time")
     private LocalDateTime endTime;
+
     @Column(name="available_seats")
     private int availableSeats;
 
--- src/main/java/com/example/ScheduleMate/utils/converters/BookingDtoUtils.java
+++ src/main/java/com/example/ScheduleMate/utils/converters/BookingDtoUtils.java
@@ -11,9 +11,9 @@ public class BookingDtoUtils {
     public static final Function<Booking, BookingDto> BOOKING_TO_BOOKING_DTO_FUNCTION = booking -> {
         BookingDto bookingDto = new BookingDto();
         bookingDto.setBookingId(booking.getId());
-        bookingDto.setClientId(booking.getClientId());
-        bookingDto.setEventId(booking.getEventId());
-        bookingDto.setSlotId(booking.getSlotId());
+//        bookingDto.setClientId(booking.getClientId());
+//        bookingDto.setEventId(booking.getEventId());
+//        bookingDto.setSlotId(booking.getSlotId());
         bookingDto.setNotes(booking.getNotes());
         bookingDto.setStatus(booking.getStatus().name()); // Convert Enum to String
         bookingDto.setProviderNotes(booking.getProviderNotes());
@@ -24,9 +24,9 @@ public class BookingDtoUtils {
     public static final Function<BookingDto, Booking> BOOKING_DTO_TO_BOOKING_FUNCTION = bookingDto -> {
         Booking booking = new Booking();
         booking.setId(bookingDto.getBookingId());
-        booking.setClientId(bookingDto.getClientId());
-        booking.setEventId(bookingDto.getEventId());
-        booking.setSlotId(bookingDto.getSlotId());
+//        booking.setClientId(bookingDto.getClientId());
+//        booking.setEventId(bookingDto.getEventId());
+//        booking.setSlotId(bookingDto.getSlotId());
         booking.setNotes(bookingDto.getNotes());
         booking.setStatus(BookingStatus.valueOf(bookingDto.getStatus())); // Convert String to Enum
         booking.setProviderNotes(bookingDto.getProviderNotes());
