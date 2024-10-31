de638d7ee37b5b84d10d2202765e92a9a88b22b4
Kalana Mihiranga <kalanamihiranga97@gmail.com>
2024-11-01 00:26:26 +0530

    feat:paginations for packages

--- src/main/java/com/example/ScheduleMate/controller/PackageController.java
+++ src/main/java/com/example/ScheduleMate/controller/PackageController.java
@@ -2,12 +2,16 @@ package com.example.ScheduleMate.controller;
 
 import com.example.ScheduleMate.dto.BusinessPackageResponse;
 import com.example.ScheduleMate.dto.PackageDto;
+import com.example.ScheduleMate.dto.ServiceDto;
 import com.example.ScheduleMate.endpoints.APIResponse;
 import com.example.ScheduleMate.entity.Packages;
 import com.example.ScheduleMate.service.PackageService;
 import com.example.ScheduleMate.utils.ResponseCode;
 import jakarta.validation.constraints.Null;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -35,5 +39,14 @@ public class PackageController {
     @GetMapping("/getAll")
     public ResponseEntity<APIResponse<List<PackageDto>>> getAllPackages() {
         return ResponseEntity.ok(new APIResponse<>(ResponseCode.SUCCESS, packageService.getAllPackages()));
+    }
+
+    @GetMapping("getpackages/{id}")
+    public ResponseEntity<APIResponse<Page<PackageDto>>> getServicesByBusiness
+            (@PathVariable Long id,
+             @RequestParam(value = "page", defaultValue = "0") int page,
+             @RequestParam(value = "size", defaultValue = "10") int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return ResponseEntity.ok(new APIResponse<>(ResponseCode.SUCCESS, packageService.getPackageListByBusinessId(id,pageable)));
     }
 }
